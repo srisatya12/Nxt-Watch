f58ee271bfc263e4a80a708e584852e945fdb9f0
Sri <srisatya1197@gmail.com>
2024-09-02 13:24:33 +0000

    Added Code

---
+++ src/ReactContext/NxtContext.js
@@ -0,0 +1,11 @@
+
+import {createContext} from 'react'
+
+const CommonContext = createContext({
+  darkMode: true,
+  changeTheme: () => {},
+  savedVideosList: [],
+  onSaveVideos: () => {},
+})
+
+export default CommonContext
